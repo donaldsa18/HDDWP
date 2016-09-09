5413667730c18b855495f8388b0b68e6f2fb757f
unknown <donaldsa18@EN208-19296.campus.up.edu>
2016-09-09 12:59:20 -0700

    Added a rotate capability to the odd magic square generator.

--- src/MagicSquaresGenerator.java
+++ src/MagicSquaresGenerator.java
@@ -67,6 +67,38 @@ public class MagicSquaresGenerator {
 					}
 				}
 			}
+			else if(rand == 0) {
+				for(int i=1;i<=n;i++) {
+					for(int j=1;j<=n;j++) {
+						//Algorithm from https://en.wikipedia.org/wiki/Magic_square#Method_for_constructing_a_magic_square_of_odd_order
+						newSquare.square[j-1][i-1] = n*( (i+j-1+(int)(n/2))%n ) + ((i+2*j-2)%n) + 1;
+					}
+				}
+			}
+			else if(rand == 1) {
+				for(int i=1;i<=n;i++) {
+					for(int j=1;j<=n;j++) {
+						//Algorithm from https://en.wikipedia.org/wiki/Magic_square#Method_for_constructing_a_magic_square_of_odd_order
+						newSquare.square[n-j][i-1] = n*( (i+j-1+(int)(n/2))%n ) + ((i+2*j-2)%n) + 1;
+					}
+				}
+			}
+			else if(rand == 2) {
+				for(int i=1;i<=n;i++) {
+					for(int j=1;j<=n;j++) {
+						//Algorithm from https://en.wikipedia.org/wiki/Magic_square#Method_for_constructing_a_magic_square_of_odd_order
+						newSquare.square[j-1][n-i] = n*( (i+j-1+(int)(n/2))%n ) + ((i+2*j-2)%n) + 1;
+					}
+				}
+			}
+			else if(rand == 3) {
+				for(int i=1;i<=n;i++) {
+					for(int j=1;j<=n;j++) {
+						//Algorithm from https://en.wikipedia.org/wiki/Magic_square#Method_for_constructing_a_magic_square_of_odd_order
+						newSquare.square[n-j][n-i] = n*( (i+j-1+(int)(n/2))%n ) + ((i+2*j-2)%n) + 1;
+					}
+				}
+			}
 			return newSquare;
 		}
 		else {
